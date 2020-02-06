fc63ee7dfd87804ca3e762fc41bc4c7102f025ad
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2020-02-06 12:21:30 -0500

    added Newwork controller class

--- Foodie/Foodie/Controllers/NetworkController.swift
+++ Foodie/Foodie/Controllers/NetworkController.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+class NetworkController {
+    
+    private let baseURL = URL(string: "foodiefun-buildweek.herokuapp.com/api")!
+    var bearer: Bearer?
+    
+    
+    func signUp(with user: Foodie1, completion: @escaping (Error?) -> ()) {
+        let signUpUrl = baseURL.appendingPathComponent("api/register")
+        
+        var request = URLRequest(url: signUpUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo:nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            }.resume()
+    }
+}
